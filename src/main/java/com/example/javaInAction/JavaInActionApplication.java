4c3518b1b9b2a7c368a73fd16365b22eca0cd577
jung <doizz@naver.com>
2022-04-13 22:03:31 +0900

    Initial commit

---
+++ src/main/java/com/example/javaInAction/JavaInActionApplication.java
@@ -0,0 +1,19 @@
+package com.example.javaInAction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaInActionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaInActionApplication.class, args);
+
+		String name = "11323";
+		System.out.println("name = " + name);
+	}
+
+
+
+
+}
---
+++ src/test/java/com/example/javaInAction/JavaInActionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javaInAction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaInActionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
